0afb885c31a561b3d29b17167b80802d37db0af1
MatthiasKi <matthias.kissel@outlook.de>
2022-10-14 15:26:39 +0200

    added a hyper parameter search regarding the hodlr depth to the hodlr approximator

--- src/structurednets/approximators/hodlr_approximator.py
+++ src/structurednets/approximators/hodlr_approximator.py
@@ -20,19 +20,24 @@ def build_hodlr_block_cluster_tree(depth: int, matrix_shape: tuple, min_block_si
 
 class HODLRApproximator(Approximator):
     def approximate(self, optim_mat: np.ndarray, nb_params_share: float):
-        # TODO add a hyperparameter search: use different block cluster tree depths
-        depth = 4
+        best_hmatrix = None
+        best_hmatrix_error = None
 
-        block_cluster_tree = build_hodlr_block_cluster_tree(depth=depth, matrix_shape=optim_mat.shape)
-        hmatrix = HMatrix(block_cluster_tree=block_cluster_tree)
-        hmatrix.find_best_leaf_approximation(optim_mat=optim_mat, nb_params_share=nb_params_share)
+        for depth in range(1, 8):
+            block_cluster_tree = build_hodlr_block_cluster_tree(depth=depth, matrix_shape=optim_mat.shape)
+            hmatrix = HMatrix(block_cluster_tree=block_cluster_tree)
+            hmatrix.find_best_leaf_approximation(optim_mat=optim_mat, nb_params_share=nb_params_share)
 
-        approx_mat_dense = hmatrix.to_dense_numpy()
+            curr_error = hmatrix.get_curr_error(optim_mat=optim_mat)
+            if best_hmatrix_error is None \
+                or curr_error < best_hmatrix_error:
+                best_hmatrix_error = curr_error
+                best_hmatrix = hmatrix
 
         res_dict = dict()
         res_dict["type"] = "HODLRApproximator"
-        res_dict["h_matrix"] = hmatrix
-        res_dict["approx_mat_dense"] = approx_mat_dense
+        res_dict["h_matrix"] = best_hmatrix
+        res_dict["approx_mat_dense"] = best_hmatrix.to_dense_numpy()
         return res_dict
 
     def get_name(self):
