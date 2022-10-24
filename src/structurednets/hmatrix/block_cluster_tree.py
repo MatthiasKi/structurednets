fixed a major bug when setting the low rank components in the h matrix components (sign error)

src/structurednets/hmatrix/block_cluster_tree.py
@@ -62,14 +62,14 @@ class BlockClusterTree:
         _, ax = plt.subplots()
         cmap = matplotlib.cm.get_cmap('Paired')
 
-        leaf_ranges = self.get_all_leaf_ranges()
+        hmatrix_components = self.get_all_hmatrix_components()
 
         max_row_idx = 0
         max_col_idx = 0
-        for leaf_range in leaf_ranges:
-            left_lower_pos = (leaf_range[1].start, -leaf_range[0].start)
-            width = leaf_range[1].stop - leaf_range[1].start
-            height = - (leaf_range[0].stop - leaf_range[0].start)
+        for hmatrix_component in hmatrix_components:
+            left_lower_pos = (hmatrix_component.col_range.start, -hmatrix_component.row_range.start)
+            width = hmatrix_component.col_range.stop - hmatrix_component.col_range.start
+            height = - (hmatrix_component.row_range.stop - hmatrix_component.row_range.start)
 
             ax.add_patch(
                 Rectangle(
@@ -80,9 +80,10 @@ class BlockClusterTree:
                     facecolor=cmap(np.random.uniform(0, 1))
                 )
             )
+            ax.text(left_lower_pos[0] + width / 2, left_lower_pos[1] + height / 2, str(hmatrix_component.get_current_nb_singular_values()))
 
-            max_row_idx = max(leaf_range[0].stop, max_row_idx)
-            max_col_idx = max(leaf_range[1].stop, max_col_idx)
+            max_row_idx = max(hmatrix_component.row_range.stop, max_row_idx)
+            max_col_idx = max(hmatrix_component.col_range.stop, max_col_idx)
 
         plt.xlim([0, max_col_idx])
         plt.ylim([-max_row_idx, 0])

tests/test_approximators.py
@@ -81,13 +81,17 @@ class ApproximatorTests(TestCase):
         self.assertTrue(np.array_equal(approx_mat_dense.shape, np.array([51, 10])), "The approximated optim_mat has the wrong shape")
 
     def test_hodlr_with_hodlr_matrix(self):
-        rand_mat = np.random.uniform(-1,1, size=(47,34))
-        approximator_1 = HODLRApproximator()
-        res = approximator_1.approximate(rand_mat, nb_params_share=0.2)
-        optim_mat = res["approx_mat_dense"]
-
+        first_dim_half = 50
+        second_dim_half = 25
+        low_rank = 8
+        optim_mat = np.zeros((2*first_dim_half, 2*second_dim_half))
+        optim_mat[:first_dim_half, :second_dim_half] = np.random.uniform(-1, 1, size=(first_dim_half, low_rank)) @ np.random.uniform(-1, 1, size=(low_rank, second_dim_half))
+        optim_mat[first_dim_half:, :second_dim_half] = np.random.uniform(-1, 1, size=(first_dim_half, low_rank)) @ np.random.uniform(-1, 1, size=(low_rank, second_dim_half))
+        optim_mat[:first_dim_half, second_dim_half:] = np.random.uniform(-1, 1, size=(first_dim_half, low_rank)) @ np.random.uniform(-1, 1, size=(low_rank, second_dim_half))
+        optim_mat[first_dim_half:, second_dim_half:] = np.random.uniform(-1, 1, size=(first_dim_half, low_rank)) @ np.random.uniform(-1, 1, size=(low_rank, second_dim_half))
+        
         approximator_2 = HODLRApproximator()
-        res = approximator_2.approximate(optim_mat, nb_params_share=0.2)
+        res = approximator_2.approximate(optim_mat, nb_params_share=0.5)
         approx_mat_dense = res["approx_mat_dense"]
         self.assertTrue(np.allclose(optim_mat, approx_mat_dense, rtol=1e-5, atol=1e-5), "The HODLR approximation algorithm should be able to fully recover a HODLR matrix")
 
@@ -100,12 +104,16 @@ class ApproximatorTests(TestCase):
         self.assertTrue(np.array_equal(approx_mat_dense.shape, np.array([51, 10])), "The approximated optim_mat has the wrong shape")
 
     def test_hedlr_with_hodlr_matrix(self):
-        rand_mat = np.random.uniform(-1,1, size=(47,34))
-        approximator_1 = HEDLRApproximator()
-        res = approximator_1.approximate(rand_mat, nb_params_share=0.2)
-        optim_mat = res["approx_mat_dense"]
-
+        first_dim_half = 50
+        second_dim_half = 25
+        low_rank = 8
+        optim_mat = np.zeros((2*first_dim_half, 2*second_dim_half))
+        optim_mat[:first_dim_half, :second_dim_half] = np.random.uniform(-1, 1, size=(first_dim_half, low_rank)) @ np.random.uniform(-1, 1, size=(low_rank, second_dim_half))
+        optim_mat[first_dim_half:, :second_dim_half] = np.random.uniform(-1, 1, size=(first_dim_half, low_rank)) @ np.random.uniform(-1, 1, size=(low_rank, second_dim_half))
+        optim_mat[:first_dim_half, second_dim_half:] = np.random.uniform(-1, 1, size=(first_dim_half, low_rank)) @ np.random.uniform(-1, 1, size=(low_rank, second_dim_half))
+        optim_mat[first_dim_half:, second_dim_half:] = np.random.uniform(-1, 1, size=(first_dim_half, low_rank)) @ np.random.uniform(-1, 1, size=(low_rank, second_dim_half))
+        
         approximator_2 = HEDLRApproximator()
-        res = approximator_2.approximate(optim_mat, nb_params_share=0.2)
+        res = approximator_2.approximate(optim_mat, nb_params_share=0.5)
         approx_mat_dense = res["approx_mat_dense"]
         self.assertTrue(np.allclose(optim_mat, approx_mat_dense, rtol=1e-5, atol=1e-5), "The HEDLR approximation algorithm should be able to fully recover a HEDLR matrix")