382be0a5544a30b1ca908d2a6c8faf5c26c35ffd
MatthiasKi <matthias.kissel@outlook.de>
2022-11-23 15:12:44 +0100

    made the hyperparameter search for the different structure approximation algorithms better comparable

--- src/structurednets/benchmarks/approximate_test_matrices.py
+++ src/structurednets/benchmarks/approximate_test_matrices.py
@@ -1,8 +1,9 @@
 import pickle 
 import numpy as np
+import time
 
 from structurednets.asset_helpers import get_all_test_matrix_dicts
-from structurednets.approximators.hmat_approximator import HMatApproximator
+from structurednets.approximators.hmat_approximator_wrapper import HMatApproximatorWrapper
 from structurednets.approximators.tl_approximator import TLApproximator
 from structurednets.approximators.lr_approximator import LRApproximator
 from structurednets.approximators.psm_approximator_wrapper import PSMApproximatorWrapper
@@ -18,22 +19,26 @@ def get_approximation_error(orig: np.ndarray, approx: np.ndarray) -> float:
 def benchmark_approximate_test_matrices(results_filepath="test_matrices_approximation_result.p"):
     nb_params_shares = np.linspace(start=0.1, stop=0.5, num=5)
     approximators = [
-        HMatApproximator(),
+        HMatApproximatorWrapper(),
         TLApproximator(),
         LRApproximator(),
-        PSMApproximatorWrapper(),
-        SSSApproximatorWrapper(),
+        PSMApproximatorWrapper(num_interpolation_steps=5, only_linear_distribution=True),
+        SSSApproximatorWrapper(num_states_steps=3),
     ]
 
     test_matrix_dicts = get_all_test_matrix_dicts()
     result = dict()
     
     for nb_params_share in nb_params_shares:
+        print("Starting Param share " + str(nb_params_share))
         result[nb_params_share] = dict()
         for approximator in approximators:
             approximator_name = approximator.get_name()
+            print("Starting approximator " + str(approximator_name))
             result[nb_params_share][approximator_name] = dict()
             for test_matrix_dict in test_matrix_dicts:
+                start_time = time.time()
+
                 test_matrix_name = test_matrix_dict["name"]
                 result[nb_params_share][approximator_name][test_matrix_name] = dict()
                 for test_matrix_idx in range(3):
@@ -48,16 +53,19 @@ def benchmark_approximate_test_matrices(results_filepath="test_matrices_approxim
                     result[nb_params_share][approximator_name][test_matrix_name][test_matrix_idx]["error"] = error
                     result[nb_params_share][approximator_name][test_matrix_name][test_matrix_idx]["nb_params"] = nb_params
                     result[nb_params_share][approximator_name][test_matrix_name][test_matrix_idx]["type"] = approximator_type
+                
+                elapsed_time = time.time() - start_time
+                print(str(test_matrix_name) + " took " + str(elapsed_time) + " seconds (" + str(approximator_name) + ", " + str(nb_params_share) + ")")
                     
-    pickle.dump(result, open(results_filepath, "wb"))
+        pickle.dump(result, open(results_filepath, "wb"))
 
 def benchmark_approximate_weight_matrices(results_filepath="weight_matrices_approximation_result.p"):
     nb_params_shares = np.linspace(start=0.1, stop=0.5, num=5)
     approximators = [
-        HMatApproximator(),
+        HMatApproximatorWrapper(),
         LRApproximator(),
-        PSMApproximatorWrapper(),
-        SSSApproximatorWrapper(),
+        PSMApproximatorWrapper(num_interpolation_steps=5, only_linear_distribution=True),
+        SSSApproximatorWrapper(num_states_steps=3),
     ]
 
     required_indices = np.arange(1000)
@@ -74,11 +82,15 @@ def benchmark_approximate_weight_matrices(results_filepath="weight_matrices_appr
     result = dict()
     
     for nb_params_share in nb_params_shares:
+        print("Starting Param share " + str(nb_params_share))
         result[nb_params_share] = dict()
         for approximator in approximators:
             approximator_name = approximator.get_name()
+            print("Starting approximator " + str(approximator_name))
             result[nb_params_share][approximator_name] = dict()
             for model_name, weight_matrix in zip(model_names, weight_matrices):
+                start_time = time.time()
+
                 result[nb_params_share][approximator_name][model_name] = dict()
                 res_dict = approximator.approximate(optim_mat=weight_matrix, nb_params_share=nb_params_share)
 
@@ -90,9 +102,16 @@ def benchmark_approximate_weight_matrices(results_filepath="weight_matrices_appr
                 result[nb_params_share][approximator_name][model_name]["error"] = error
                 result[nb_params_share][approximator_name][model_name]["nb_params"] = nb_params
                 result[nb_params_share][approximator_name][model_name]["type"] = approximator_type
+
+                if nb_params_share == 0.2:
+                    # We store this dict, since we will need it for fine tuning
+                    result[nb_params_share][approximator_name][model_name]["res_dict"] = res_dict
+
+                elapsed_time = time.time() - start_time
+                print(str(model_name) + " took " + str(elapsed_time) + " seconds (" + str(approximator_name) + ", " + str(nb_params_share) + ")")
                     
     pickle.dump(result, open(results_filepath, "wb"))
 
 if __name__ == "__main__":
-    benchmark_approximate_test_matrices()
-    #benchmark_approximate_weight_matrices()
+    #benchmark_approximate_test_matrices()
+    benchmark_approximate_weight_matrices()
