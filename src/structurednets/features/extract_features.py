c8620499f2ea6155353022f0fbe66111939215e6
MatthiasKi <matthias.kissel@outlook.de>
2022-11-29 14:48:38 +0100

    made some code improvements, and outsource the extract_images function

--- src/structurednets/features/extract_features.py
+++ src/structurednets/features/extract_features.py
@@ -15,6 +15,12 @@ from structurednets.models.mobilenetv2 import MobilenetV2
 from structurednets.models.resnet18 import Resnet18
 from structurednets.models.vgg16 import VGG16
 
+def get_device(use_gpu=True):
+    if use_gpu:
+        return torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    else:
+        return 'cpu'
+
 def get_label_file_lines(path_to_label_file: str):
     with open(path_to_label_file, "r") as f:
         label_lines = f.readlines()
@@ -48,11 +54,7 @@ def get_preprocessing_transformation():
     return img_transformation
 
 def extract_validation_features(output_folder_path: str, validation_folder_path: str, model_class: VisionModel, label_filepath: str, use_gpu=True):
-    if use_gpu:
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    else:
-        device = 'cpu'
-
+    device = get_device(use_gpu=use_gpu)
     output_filename = get_features_output_filename(model_class, label_filepath)
 
     if not os.path.isdir(output_folder_path):
@@ -88,44 +90,73 @@ def extract_validation_features(output_folder_path: str, validation_folder_path:
     pickle.dump([X, y, y_pred], open(os.path.join(output_folder_path, output_filename), "wb"))
 
 def extract_features(output_folder_path: str, training_folder_path: str, model_class: VisionModel, label_filepath: str, use_gpu=True):
-    if use_gpu:
-        device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-    else:
-        device = 'cpu'
-
     output_filename = get_features_output_filename(model_class, label_filepath)
-
     if not os.path.isdir(output_folder_path):
         os.mkdir(output_folder_path)
 
-    required_indices = get_required_indices(label_filepath)
-
-    model = model_class(output_indices=required_indices)
-    img_transformation = get_preprocessing_transformation()
-
-    X = []
-    y = []
-    y_pred = []
+    X = None
+    y = None
+    y_pred = None
 
     label_lines = get_label_file_lines(path_to_label_file=label_filepath)
     for line_i, line in enumerate(label_lines):
         folder_name = line.split(" ")[0]
         image_names = os.listdir(os.path.join(training_folder_path, folder_name))
-        for image_name in image_names:
-            img = Image.open(os.path.join(training_folder_path, folder_name, image_name)).convert("RGB")
-            tensor_img = img_transformation(img)
-            batched_img = torch.unsqueeze(tensor_img, 0)
-            batched_img = batched_img.to(device)
+        image_paths = [os.path.join(training_folder_path, folder_name, image_name) for image_name in image_names]
 
-            X.append(model.get_features_for_batch(batched_img).cpu().detach().numpy())
-            y.append(line_i)
-            y_pred.append(model.predict_pretrained_with_argmax(batched_img).cpu().detach().numpy())
+        X_append, y_append, y_pred_append = extract_images(
+            image_paths=image_paths,
+            model_class=model_class,
+            label_filepath=label_filepath,
+            label=line_i,
+            use_gpu=use_gpu
+        )
+
+        if X is None:
+            X = X_append
+            y = y_append
+            y_pred = y_pred_append
+        else:
+            X = np.concatenate([X, X_append], axis=0)
+            y = np.concatenate([y, y_append], axis=0)
+            y_pred = np.concatenate([y_pred, y_pred_append], axis=0)
+
+    pickle.dump([X, y, y_pred], open(os.path.join(output_folder_path, output_filename), "wb"))
+
+def extract_images(
+    image_paths: list,
+    model_class: VisionModel, 
+    label_filepath: str,
+    label: int,
+    use_gpu=True,
+):
+    device = get_device(use_gpu=use_gpu)
+    img_transformation = get_preprocessing_transformation()
+    required_indices = get_required_indices(label_filepath)
+    model = model_class(output_indices=required_indices)
+
+    X = []
+    y = []
+    y_pred = []
+
+    for image_path in image_paths:
+        img = Image.open(image_path).convert("RGB")
+        tensor_img = img_transformation(img)
+        batched_img = torch.unsqueeze(tensor_img, 0)
+        batched_img = batched_img.to(device)
+
+        # NOTE: We do not put the images into one batch together (which would make the computation much faster), because
+        # the images can have different sizes (I trade better features for slower computation time here)
+
+        X.append(model.get_features_for_batch(batched_img).cpu().detach().numpy())
+        y.append(label)
+        y_pred.append(model.predict_pretrained_with_argmax(batched_img).cpu().detach().numpy())
 
     X = np.array(X)
     y = np.array(y)
     y_pred = np.array(y_pred)
 
-    pickle.dump([X, y, y_pred], open(os.path.join(output_folder_path, output_filename), "wb"))
+    return X, y, y_pred
 
 if __name__ == "__main__":
     # ---------------------
