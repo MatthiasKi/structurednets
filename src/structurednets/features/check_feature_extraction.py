d8be01fe221b66d7fdc200485065f32a9baf7e8d
MatthiasKi <matthias.kissel@outlook.de>
2022-12-19 14:24:28 +0100

    added a printout for the total accuracy

--- src/structurednets/features/check_feature_extraction.py
+++ src/structurednets/features/check_feature_extraction.py
@@ -14,7 +14,7 @@ from structurednets.models.vgg16 import VGG16
 
 def check_features(feature_filepath: str, model_class: VisionModel, label_filepath: str):
     required_indices = get_required_indices(label_filepath)
-    model = model_class(required_indices)
+    model = model_class(required_indices, use_gpu=False)
     output_mat = model.get_optimization_matrix()
 
     X, y, y_pred = load_features(path_to_feature_file=feature_filepath)
@@ -24,18 +24,22 @@ def check_features(feature_filepath: str, model_class: VisionModel, label_filepa
 
     accuracies = []
     y_values = np.unique(y)
+    total_nb_correct_predictions = 0
     for y_value in y_values:
         mask = y == y_value
         curr_ground_truth = y[mask]
         curr_predictions = y_pred[mask]
         accuracies.append(float(np.sum(curr_ground_truth == curr_predictions)) / np.sum(mask))
+        total_nb_correct_predictions += np.sum(curr_ground_truth == curr_predictions)
         print("Accuracy for class " + str(y_value) + ": " + str(accuracies[-1]))
+    total_accuracy = float(total_nb_correct_predictions) / len(y)
 
     print("---------------")
     print("Min accuracy: " + str(np.min(accuracies)))
     print("Max accuracy: " + str(np.max(accuracies)))
     print("Mean Accuracy: " + str(np.mean(accuracies)) + "+-" + str(np.std(accuracies)))
     print("Median Accuracy: " + str(np.median(accuracies)))
+    print("Total Accuracy: " + str(total_accuracy))
 
 if __name__ == "__main__":
     feature_filepath = "/path/to/AlexNet_animal_features.p"
